e6fc6accf2d07faf46ae99999097b302d04f3aae
Daniel Klosa <klosa@uni-bremen.de>
2025-02-21 12:56:14 +0100

    Raise exceptions when coordinates are not found by mapbox or something else goes wrong

--- mapbox.py
+++ mapbox.py
@@ -6,8 +6,22 @@ from dotenv import load_dotenv
 load_dotenv()
 API_TOKEN = os.getenv("MAPBOX_API_TOKEN")
 
+class CoordinatesNotFoundException(Exception):
+    def __init__(self, address):
+        self.message = f"Coordinates were not found for address {address}"
+        super().__init__(self.message)
+
 def get_coordinates_from_address(address):
-    url = f"https://api.mapbox.com/search/geocode/v6/forward?q={address}&access_token={API_TOKEN}"
+    url = f"https://api.mapbox.com/search/geocode/v6/forward?q={address}&limit=1&access_token={API_TOKEN}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    if len(data["features"]) == 0:
+        raise CoordinatesNotFoundException(address)
+    feature = data["features"][0]
+    if not "geometry" in feature.keys():
+        raise Exception("Something went wrong when calling Mapbox API.")
+    coordinates = feature["geometry"]["coordinates"]
+    return coordinates
     response = requests.get(url)
     data = json.loads(response.text)
     return data
